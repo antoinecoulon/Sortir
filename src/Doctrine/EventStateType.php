6dfcbf78933121cc96f64546f2a25b2ee9d0097d
Antoine COULON <a.antoinecoulon@gmail.com>
2025-02-25 14:19:16 +0100

    Fix EventState method to get value

--- src/Doctrine/EventStateType.php
+++ src/Doctrine/EventStateType.php
@@ -6,6 +6,7 @@ namespace App\Doctrine;
 use App\Enum\EventState;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\StringType;
+use InvalidArgumentException;
 
 class EventStateType extends StringType
 {
@@ -25,10 +26,16 @@ class EventStateType extends StringType
         throw new \InvalidArgumentException("Invalid state value: $value");
     }
 
-    public function convertToPHPValue($value, AbstractPlatform $platform)
-    {
-        return EventState::from($value);
+    public function convertToPHPValue($value, AbstractPlatform $platform) {
+        foreach (EventState::cases() as $state) {
+            if ($state->name === $value) {
+                return $state;
+            }
+        }
+        throw new InvalidArgumentException("État d'événement invalide: $value");
     }
+
+
 
     public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
     {
