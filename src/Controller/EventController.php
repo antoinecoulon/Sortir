fc44a0759f83cdb61fa32b150ad4d3e4a63cf654
Antoine COULON <a.antoinecoulon@gmail.com>
2025-02-25 14:18:40 +0100

    Update app_event

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,16 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class EventController extends AbstractController
 {
-    #[Route('/', name: 'app_event')]
+    private readonly EventRepository $eventRepository;
+    private readonly EntityManagerInterface $em;
+    public function __construct(EventRepository $eventRepository, EntityManagerInterface $em) {
+        $this->em = $em;
+        $this->eventRepository = $eventRepository;
+    }
+
+    #[Route('/', name: 'app_event', methods: ['GET'])]
     public function index(): Response
     {
+
         return $this->render('event/index.html.twig', [
+            'events' => $this->eventRepository->findAll(),
         ]);
     }
 }
