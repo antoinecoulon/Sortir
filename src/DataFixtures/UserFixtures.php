5b2c3d6a7e4db495c5c2813a432e758f8c42b288
Justine44444 <137166047+Justine44444@users.noreply.github.com>
2025-02-27 15:57:50 +0100

    fixtures done

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Site;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class UserFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+       $faker = Factory::create('fr_FR');
+
+       $role = [
+           ['ROLE_USER'],
+           ['ROLE_ADMIN']
+       ];
+
+       for ($i = 0; $i < 10; $i++) {
+           $user = new User();
+
+           $user->setPseudo($faker->userName());
+           $user->setName($faker->lastName());
+           $user->setFirstName($faker->firstName());
+           $user->setPassword($faker->password());
+
+           $phoneNumber = $faker->numerify('0#########'); // Génère un numéro avec 10 chiffres
+           $user->setPhone($phoneNumber);
+
+           $user->setEmail($faker->email());
+           $user->setRoles($role[array_rand($role)]);
+           $user->setPhoto($faker->image());
+           $user->setIsActive($faker->boolean());
+
+           $site = $this->getReference('site_' . rand(0, 3), Site::class);
+           $user->setSite($site);
+
+           $manager->persist($user);
+
+           $this->setReference('user_', $user);
+
+       }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            SiteFixtures::class
+        ];
+    }
+}
