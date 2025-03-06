a147a13a76d1e9e334526a21fa8b092d540806bb
AlexRovere <75780598+AlexRovere@users.noreply.github.com>
2025-03-06 17:18:56 +0100

    add wait contentload map

--- assets/js/leaflet.js
+++ assets/js/leaflet.js
@@ -2,13 +2,28 @@ import "leaflet"
 import 'leaflet/dist/leaflet.css';
 
 document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('load', () => {
+        initMap();
+    });
+
+    initMap();
+});
+
+function initMap() {
+    if (!document.getElementById('map')) return;
+
     const icon = L.icon({
         iconUrl: markerLocation,
         iconSize: [28, 32],
         popupAnchor: [-3, -15]
-    })
+    });
+
+    if (window.leafletMap) {
+        window.leafletMap.remove();
+    }
 
     let map = L.map('map').setView([latitude, longitude], 13);
+    window.leafletMap = map;
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
@@ -16,6 +31,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }).addTo(map);
 
     let marker = L.marker([latitude, longitude], {icon: icon}).addTo(map);
-
     marker.bindPopup(locationName).openPopup();
-});
+
+    setTimeout(() => {
+        map.invalidateSize();
+    }, 500);
+}
